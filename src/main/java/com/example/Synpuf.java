1027fc6846cb5e5e76a9a55d4a249e9671f42c08
saiba09 <saiba_sheikh@persistent.co.in>
2017-04-07 11:47:50 +0530

    for all columns update

--- src/main/java/com/example/Synpuf.java
+++ src/main/java/com/example/Synpuf.java
@@ -23,10 +23,26 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 public class Synpuf
 {
- private static final byte[] FAMILY = Bytes.toBytes("beneficiary-summary");
-  private static final byte[] beneficiry_id = Bytes.toBytes("beneficiry_id");
+  private static final byte[] FAMILY = Bytes.toBytes("beneficiry-summary");
+    private static final byte[] beneficiry_id = Bytes.toBytes("beneficiry_id");
     private static final byte[] death_date = Bytes.toBytes("death_date");
-   private static long row_id = 0;
+    private static final byte[] SEX = Bytes.toBytes("sex");
+    private static final byte[] ESRDI = Bytes.toBytes("esrdi");
+    private static final byte[] state_code = Bytes.toBytes("state_code");
+    private static final byte[] ANUAL_MEDICLAIM_INPATIENT = Bytes.toBytes("anual_mediclaim_inpatient");
+    private static final byte[] ANUAL_MEDICLAIM_OUTPATIENT = Bytes.toBytes("anual_mediclaim_outpatient");
+    private static final byte[] SP_ALZHDMTA = Bytes.toBytes("SP_ALZHDMTA");
+    private static final byte[] SP_CHF = Bytes.toBytes("SP_CHF");
+    private static final byte[] SP_CHRNKIDN = Bytes.toBytes("SP_CHRNKIDN");
+    private static final byte[] SP_CNCR = Bytes.toBytes("SP_CNCR");
+    private static final byte[] SP_COPD = Bytes.toBytes("SP_COPD");
+    private static final byte[] SP_DEPRESSN = Bytes.toBytes("SP_DEPRESSN");
+    private static final byte[] SP_DIABETES = Bytes.toBytes("SP_DIABETES");
+    private static final byte[] SP_ISCHMCHT = Bytes.toBytes("SP_ISCHMCHT");
+    private static final byte[] SP_OSTEOPRS = Bytes.toBytes("SP_OSTEOPRS");
+    private static final byte[] SP_RA_OA = Bytes.toBytes("SP_RA_OA");
+    private static final byte[] SP_STRKETIA = Bytes.toBytes("SP_STRKETIA");
+    private static long row_id = 1;
     //private static final byte[] SEX = Bytes.toBytes("sex");
 
 static final DoFn<String, Mutation> MUTATION_TRANSFORM = new DoFn<String, Mutation>() {
@@ -46,9 +62,25 @@ static final DoFn<String, Mutation> MUTATION_TRANSFORM = new DoFn<String, Mutati
    				Put put_object = new Put(Bytes.toBytes(row_id));
 				row_id = row_id +1;	
      			    	byte[] data = Bytes.toBytes( parts[0]);
-   					put_object.addColumn(FAMILY, beneficiry_id,data);
-   					put_object.addColumn(FAMILY, death_date, Bytes.toBytes(parts[2]));
-					c.output(put_object);
+   			put_object.addColumn(FAMILY, beneficiry_id,data));
+ 			put_object.addColumn(FAMILY, death_date, Bytes.toBytes(parts[2])));
+			put_object.addColumn(FAMILY, SEX, Bytes.toBytes(parts[3])));
+			put_object.addColumn(FAMILY, ESRDI, Bytes.toBytes(parts[5])));
+			put_object.addColumn(FAMILY, state_code, Bytes.toBytes(parts[6])));
+			put_object.addColumn(FAMILY, SP_ALZHDMTA, Bytes.toBytes(parts[12])));
+			put_object.addColumn(FAMILY, SP_CHF, Bytes.toBytes(parts[13])));
+			put_object.addColumn(FAMILY, SP_CHRNKIDN, Bytes.toBytes(parts[14])));
+			put_object.addColumn(FAMILY, SP_CNCR, Bytes.toBytes(parts[15])));
+			put_object.addColumn(FAMILY, SP_COPD, Bytes.toBytes(parts[16])));
+			put_object.addColumn(FAMILY, SP_DEPRESSN, Bytes.toBytes(parts[17])));
+			put_object.addColumn(FAMILY, SP_DIABETES, Bytes.toBytes(parts[18])));
+			put_object.addColumn(FAMILY, SP_ISCHMCHT, Bytes.toBytes(parts[19])));
+			put_object.addColumn(FAMILY, SP_OSTEOPRS, Bytes.toBytes(parts[20])));
+			put_object.addColumn(FAMILY, SP_RA_OA, Bytes.toBytes(parts[21])));
+			put_object.addColumn(FAMILY, SP_STRKETIA, Bytes.toBytes(parts[22])));
+			put_object.addColumn(FAMILY, ANUAL_MEDICLAIM_INPATIENT, Bytes.toBytes(parts[23])));
+			put_object.addColumn(FAMILY, ANUAL_MEDICLAIM_OUTPATIENT, Bytes.toBytes(parts[27])));
+			c.output(put_object);
   }
 };
 		
