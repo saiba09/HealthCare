0cdf99fb285465ed4b24bbfa8684beda67e46615
saiba09 <saiba_sheikh@persistent.com>
2017-04-07 11:04:44 +0530

    changes2

--- src/main/java/com/example/mihin.java
+++ src/main/java/com/example/mihin.java
@@ -47,11 +47,11 @@ public class  mihin
     private static long row_id = 0;
     //private static final byte[] SEX = Bytes.toBytes("sex");
 
- static final DoFn<String, String> MUTATION_TRANSFORM = new DoFn<String, String>() {
+ static final DoFn<String, PTransform> MUTATION_TRANSFORM = new DoFn<String, PTransform>() {
   	private static final long serialVersionUID = 1L;
 
   @Override
-  public void processElement(DoFn<String, String>.ProcessContext c) throws Exception {
+  public void processElement(DoFn<String, PTransform>.ProcessContext c) throws Exception {
 
   			String line = c.element();
 		 	// CSVParser csvParser = new CSVParser();
@@ -66,7 +66,7 @@ public class  mihin
 
    	// 				 put_object.addColumn(FAMILY, beneficiry_id,data[0]));
  			// 		 put_object.addColumn(FAMILY, death_date, Bytes.toBytes(parts[2])));
-   					 c.output(line);
+   					 c.output(c.element());
 
 
   }
