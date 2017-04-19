a5545453aed73da03f17920b7e026f738135ddcb
saiba09 <saiba_sheikh@persistent.co.in>
2017-04-19 09:42:49 +0530

    seq test

--- src/main/java/com/example/mihin.java
+++ src/main/java/com/example/mihin.java
@@ -86,13 +86,15 @@ public class mihin
 		// Then create the pipeline.
 		Pipeline p = Pipeline.create(options);
  		CloudBigtableIO.initializeForWrite(p);
-		p.apply(TextIO.Read.from("gs://mihin-data/Patient_entry.txt")).apply(ParDo.of(FORMAT_JSON)).apply(TextIO.Write.to("gs://mihin-data/formatedPatientGen.json"));
- 		PipelineResult result=p.run();
-		if(result.getState().toString().equals("DONE") ){
-			p.apply(TextIO.Read.from("gs://mihin-data/formatedPatientGen.json")).apply(ParDo.of(MUTATION_TRANSFORM)).apply(CloudBigtableIO.writeToTable(config));
+		PCollection<String> lines = p.apply(TextIO.Read.from("gs://mihin-data/Patient_entry.txt"))
+		PCollection<String> fields = lines.apply(ParDo.of(FORMAT_JSON));
+			//.apply(TextIO.Write.to("gs://mihin-data/formatedPatientGen.json"));
+ 		
+		
+			//p.apply(TextIO.Read.from("gs://mihin-data/formatedPatientGen.json"))
+			feilds.apply(ParDo.of(MUTATION_TRANSFORM)).apply(CloudBigtableIO.writeToTable(config));
 			p.run();
-		}
-	
+		}	
      		//.apply(TextIO.Write.to("gs://mihin-data/temp.txt"));
 
 		
