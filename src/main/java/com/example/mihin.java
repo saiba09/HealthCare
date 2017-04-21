10eea7add941e52fa67d3b1a8cc57661c35a7ba7
saiba09 <saiba_sheikh@persistent.co.in>
2017-04-21 12:17:54 +0530

    w1.4

--- src/main/java/com/example/mihin.java
+++ src/main/java/com/example/mihin.java
@@ -48,6 +48,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.io.FileWriter;
 public class mihin
 {
 	private static final byte[] FAMILY = Bytes.toBytes("cf1");
