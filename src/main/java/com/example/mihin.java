6625a4f514ffc00e1ca71540ce48415f16a12e07
saiba09 <saiba_sheikh@persistent.co.in>
2017-04-21 16:15:44 +0530

    oop added

--- src/main/java/com/example/mihin.java
+++ src/main/java/com/example/mihin.java
@@ -1,166 +1,119 @@
 package com.example;
-import com.google.cloud.dataflow.sdk.transforms.*;
-import com.google.cloud.dataflow.sdk.PipelineResult;
-import com.google.cloud.dataflow.sdk.PipelineResult.State;
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.io.TextIO;
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions;
 import com.google.cloud.dataflow.sdk.runners.BlockingDataflowPipelineRunner;
-import com.google.cloud.dataflow.sdk.options.Default;
-import com.google.cloud.dataflow.sdk.options.DefaultValueFactory;
-import com.google.cloud.dataflow.sdk.options.Description;
-import com.google.cloud.dataflow.sdk.options.PipelineOptions;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
-import com.google.cloud.dataflow.sdk.util.gcsfs.GcsPath;
-import com.google.cloud.dataflow.sdk.values.PCollection;
-import com.opencsv.CSVParser;
+import com.google.cloud.dataflow.sdk.values.PDone;
 import java.io.IOException;
-import com.google.appengine.api.urlfetch.HTTPResponse;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import com.google.cloud.bigtable.dataflow.CloudBigtableIO;
-import com.google.cloud.bigtable.dataflow.CloudBigtableOptions;
 import com.google.cloud.bigtable.dataflow.CloudBigtableScanConfiguration;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 import java.util.HashMap;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.util.Preconditions;
-import com.google.api.services.storage.Storage;
-import com.google.cloud.dataflow.sdk.coders.StringUtf8Coder;
-import com.google.api.client.json.JsonFactory;	
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.InputStreamContent;
-import com.google.api.services.storage.model.StorageObject;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.io.FileWriter;
+import com.utils.*;
 public class mihin
 {
-	private static final byte[] FAMILY = Bytes.toBytes("cf1");
-        private static final byte[] bday = Bytes.toBytes("bday");
-        private static final byte[] gender = Bytes.toBytes("gender");
-	private static long row_id = 1;
-	public static StorageObject uploadSimple(Storage storage, String bucketName, String objectName,
-     		 InputStream data, String contentType) throws IOException {
-    		InputStreamContent mediaContent = new InputStreamContent(contentType, data);
-    		Storage.Objects.Insert insertObject = storage.objects().insert(bucketName, null, mediaContent)
-       		 .setName(objectName);
-    // The media uploader gzips content by default, and alters the Content-Encoding accordingly.
-    // GCS dutifully stores content as-uploaded. This line disables the media uploader behavior,
-    // so the service stores exactly what is in the InputStream, without transformation.
-    		insertObject.getMediaHttpUploader().setDisableGZipContent(true);
-    		return insertObject.execute();
-  }
-	static boolean getFile(String BUCKET_NAME , String FILENAME) {
-		String fileName="Patient_entry_program.json";
-		String OBJECT_NAME = "PatientFormated.json";
-		try{
-			HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        		GoogleCredential credential = GoogleCredential.getApplicationDefault();
-        		Storage storage = new Storage.Builder(httpTransport, jsonFactory, credential)
-        		.setApplicationName("Google-ObjectsListExample/1.0").build();
-         		Storage.Objects.Get obj = storage.objects().get(BUCKET_NAME, FILENAME);
-         		HttpResponse response = obj.executeMedia();
-			String file=response.parseAsString();
-			JSONParser parser = new JSONParser();
-                        Object obj1 = parser.parse(file);
-	       	        JSONObject jsonObject = (JSONObject) obj1;
-	    		//result = (jsonObject.toString());
-			
-			StorageObject writeObject = uploadSimple(storage, BUCKET_NAME, OBJECT_NAME, new ByteArrayInputStream(
-        (jsonObject.toString()).getBytes("UTF-8")), "text/plain");
-			return true;
-		}
-		catch(Exception e){
-			System.out.println(e);
-			
-		}
-		return false;
-	}
-	static final DoFn<String, Mutation> MUTATION_TRANSFORM = new DoFn<String, Mutation>() {
- 		@Override
+	private static final String BUCKET_NAME = "mihin-data";
+    private static final FormatFile fileFormater = new FormatFile();
+
+	
+	public static class Patient_entry{
+		private static final byte[] FAMILY = Bytes.toBytes("ColumnFamily1");
+	    private static final byte[] BIRTHDATE = Bytes.toBytes("birthdate");
+	    private static final byte[] GENDER = Bytes.toBytes("gender");
+	    private static final byte[] CITY = Bytes.toBytes("city");
+	    private static final byte[] NAME = Bytes.toBytes("name");
+	    private static final byte[] P_ID = Bytes.toBytes("p_id");
+	    private static final byte[] POSTALCODE = Bytes.toBytes("postalcode");
+	    private static final byte[] STATE = Bytes.toBytes("state");
+
+	    private static long row_id = 1;
+	    static final DoFn<String, Mutation> MUTATION_TRANSFORM = new DoFn<String, Mutation>() {
+ 		    
+		/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+		@SuppressWarnings("unused")
+		@Override
     		public void processElement(DoFn<String, Mutation>.ProcessContext c) throws IOException{
       			String line = c.element();
-			 JSONParser parser = new JSONParser();
-			 try {
-				Object obj = parser.parse(line);
-				JSONObject jsonObject = (JSONObject) obj;
-			 	JSONArray resource = (JSONArray) jsonObject.get("resources");
-				 Put put_object = null ;
-				for (int i = 0; i < 100; i++) {
-				        put_object = new Put(Bytes.toBytes(row_id));
-				        row_id = row_id +1;
-            				JSONObject jsonObject1 = (JSONObject) parser.parse(resource.get(i).toString());
-      					HashMap map = (HashMap) jsonObject1.get("resource");
-					put_object.addColumn(FAMILY, bday, Bytes.toBytes(map.get("birthDate").toString()));
-					put_object.addColumn(FAMILY, gender, Bytes.toBytes(map.get("gender").toString()));
-					c.output(put_object);
+      			JSONParser parser = new JSONParser();
+      			 try {
+      				Object object = parser.parse(line);
+      				JSONObject jsonObject = (JSONObject) object;
+      			 	JSONArray resource = (JSONArray) jsonObject.get("resources");
+      				Put put_object = null ;
+      				String patientName = "";
+      				String city = "";
+      				String state = "";
+      				String postalCode ="";
+      				for (int i = 0; i < 100; i++) {
+      				    put_object = new Put(Bytes.toBytes(row_id));
+      				    row_id = row_id +1;
+      	        		JSONObject jsonObject1 = (JSONObject) parser.parse(resource.get(i).toString());
+      	  				@SuppressWarnings("rawtypes")
+						HashMap map = (HashMap) jsonObject1.get("resource");
+      	  			    JSONArray FullnameArray  = (JSONArray) map.get("name");
+        		 	    JSONObject nameObject  = (JSONObject) parser.parse(FullnameArray.get(0).toString());
+        			    JSONArray nameArray = (JSONArray)(nameObject.get("given"));
+        			    System.out.println(nameArray);
+        			    if (nameArray.size() == 2) {
+    						patientName = (nameArray.get(0) +" "+nameArray.get(1));
+    					}
+        			    if (nameArray.size() == 1) {
+    					patientName = (nameArray.get(0)).toString();
+    				    }
+        			    if ( map.get("address") != null) {
+            			    JSONArray addressArray  = (JSONArray) map.get("address");
+            				JSONObject addressObject  = (JSONObject) parser.parse(((JSONArray) map.get("address")).get(0).toString());
+                			city = (addressObject.get("city")).toString();
+                			state = (addressObject.get("state")).toString();
+                			postalCode = (addressObject.get("postalCode")).toString();
+        				}
+      					put_object.addColumn(FAMILY, P_ID, Bytes.toBytes(map.get("id").toString()));
+      	  				put_object.addColumn(FAMILY, BIRTHDATE, Bytes.toBytes(map.get("birthDate").toString()));
+      					put_object.addColumn(FAMILY, GENDER, Bytes.toBytes(map.get("gender").toString()));
+      					put_object.addColumn(FAMILY, P_ID, Bytes.toBytes(map.get("").toString()));
+      					put_object.addColumn(FAMILY, NAME, Bytes.toBytes(patientName));
+      					put_object.addColumn(FAMILY, CITY, Bytes.toBytes(city));
+      					put_object.addColumn(FAMILY, STATE, Bytes.toBytes(state));
+      					put_object.addColumn(FAMILY, POSTALCODE, Bytes.toBytes(postalCode));
 
-			 }
-			 }
-			catch (Exception e) {
-            e.printStackTrace();
-        }
+      					c.output(put_object);
+      				}
+      			 }
+      			catch (Exception e) {
+      	        e.printStackTrace();
+      	    }
     		}
 	};
-// 	public static class ProcessFile extends PTransform<PCollection<String>, PCollection<String>> {
+	}
 
-//     	@Override
-//     	public PCollection<String> apply(PCollection<String> inputFile) {
-//      	 PCollection<String> formatedFile = inputFile.apply(
-//          ParDo.of(FORMAT_JSON));
-//          return formatedFile;
-//     }
-//   }
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) 
 	{
-	
 		// Start by defining the options for the pipeline.
 		CloudBigtableScanConfiguration config = new CloudBigtableScanConfiguration.Builder().withProjectId("healthcare-12").withInstanceId("hc-dataset").withTableId("mihin-test1").build();
 		DataflowPipelineOptions options = PipelineOptionsFactory.create().as(DataflowPipelineOptions.class);
 		options.setRunner(BlockingDataflowPipelineRunner.class);
 		options.setProject("healthcare-12");
-		String BUCKET_NAME = "mihin-data";
-         	String FILENAME = "Patient_entry.txt";
-		// The 'gs' URI means that this is a Google Cloud Storage path
 		options.setStagingLocation("gs://mihin-data/staging1");
-		Boolean status = getFile(BUCKET_NAME , FILENAME);
-		// Then create the pipeline.
-		if(status){
 		Pipeline p = Pipeline.create(options);
  		CloudBigtableIO.initializeForWrite(p);
-		//PCollection<String> FormatedFile = p.apply(Create.of(JSON)).setCoder(StringUtf8Coder.of()) ;
-		p.apply(TextIO.Read.from("gs://mihin-data/PatientFormated.json")).apply(ParDo.of(MUTATION_TRANSFORM))
-			.apply(CloudBigtableIO.writeToTable(config));
-			//FormatedFile.apply(TextIO.Write.to("gs://mihin-data/formatedPatientGen.json"));
- 			//p.apply(TextIO.Read.from("gs://mihin-data/formatedPatientGen.json"))
- 			p.run();
+		if(fileFormater.getFile(BUCKET_NAME, "Patient_entry.txt", "PatientFormated.json")){
+				PDone apply = p.apply(TextIO.Read.from("gs://mihin-data/PatientFormated.json")).apply(ParDo.of(Patient_entry.MUTATION_TRANSFORM)).apply(CloudBigtableIO.writeToTable(config));
+			    p.run();
 		}
 		}	
-     		//.apply(TextIO.Write.to("gs://mihin-data/temp.txt"));
-
-		
-
-		//PCollection<String> lines=p.apply(TextIO.Read.from("gs://synpuf-data/DE1_0_2008_Beneficiary_Summary_File_Sample_1.csv"))
-		//PCollection<String> fields = lines.apply(ParDo.of(new ExtractFieldsFn()));
-		//p.apply(TextIO.Write.to("gs://synpuf-data/temp.txt"));
-	
-	
+     			
 }
---
+++ src/main/java/com/utils/FormatFile.java
@@ -0,0 +1,51 @@
+package com.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.storage.Storage;
+import com.google.api.services.storage.model.StorageObject;
+
+public class FormatFile {
+	
+	public StorageObject uploadSimple(Storage storage, String bucketName, String objectName,InputStream data, String contentType) throws IOException {
+   		InputStreamContent mediaContent = new InputStreamContent(contentType, data);
+   		Storage.Objects.Insert insertObject = storage.objects().insert(bucketName, null, mediaContent).setName(objectName);
+   		insertObject.getMediaHttpUploader().setDisableGZipContent(true);
+   		return insertObject.execute();
+ }
+	public boolean getFile(String BUCKET_NAME , String SOURCE_FILE , String DESTINATION_FILE) {
+		try{
+			HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+       		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+       		GoogleCredential credential = GoogleCredential.getApplicationDefault();
+       		Storage storage = new Storage.Builder(httpTransport, jsonFactory, credential)
+       		.setApplicationName("MihinDataFormating/v1").build();
+        	Storage.Objects.Get obj = storage.objects().get(BUCKET_NAME, SOURCE_FILE);
+        	HttpResponse response = obj.executeMedia();
+			String file=response.parseAsString();
+			JSONParser parser = new JSONParser();
+            Object obj1 = parser.parse(file);
+	       	JSONObject jsonObject = (JSONObject) obj1;
+	     // result = (jsonObject.toString());
+			uploadSimple(storage, BUCKET_NAME, DESTINATION_FILE, new ByteArrayInputStream((jsonObject.toString()).getBytes("UTF-8")), "text/plain");
+			return true;
+		}
+		catch(Exception e){
+			System.out.println(e);
+			
+		}
+		return false;
+	}
+}
