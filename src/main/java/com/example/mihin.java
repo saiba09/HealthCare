20f6047ea4155a94f322ba7a47cce33c07a0c9f6
saiba09 <saiba_sheikh@persistent.co.in>
2017-04-24 11:56:32 +0530

    test2.2

--- src/main/java/com/example/mihin.java
+++ src/main/java/com/example/mihin.java
@@ -76,7 +76,7 @@ public class mihin
 				  put_object.addColumn(FAMILY, CITY, Bytes.toBytes(city));
 				  put_object.addColumn(FAMILY, STATE, Bytes.toBytes(state));
                			  put_object.addColumn(FAMILY, POSTALCODE, Bytes.toBytes(postalCode));
-				  put_object.addColumn(FAMILY, NAME, Bytes.toBytes(name));
+				  put_object.addColumn(FAMILY, NAME, Bytes.toBytes(patientName));
 				  put_object.addColumn(FAMILY, P_ID, Bytes.toBytes(map2.get("id").toString()));
       	  			  put_object.addColumn(FAMILY, BIRTHDATE, Bytes.toBytes(map2.get("birthDate").toString()));
       				  put_object.addColumn(FAMILY, GENDER, Bytes.toBytes(map2.get("gender").toString()));
